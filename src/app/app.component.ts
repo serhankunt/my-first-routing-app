e50a217336a1c27acbbcd3804c75be302580166d
serhankunt <hserhan006@gmail.com>
2023-10-19 13:21:15 +0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <ul>
+    <li routerLink="/">Home</li>
+    <li routerLink="/about">About</li>
+    <li routerLink="/contact">Contact</li>
+  </ul>
+  <router-outlet></router-outlet>
+  `
 })
 export class AppComponent {
   title = 'my-first-routing-app';
